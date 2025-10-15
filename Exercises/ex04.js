3fa65ac8b515298a3855c3461edea077af9da087
akaarons <akaarons@ucsc.edu>
2025-10-14 18:49:34 -0700

    ex04

---
+++ Exercises/ex04.js
@@ -0,0 +1,21 @@
+
+let totalNumber=0;
+
+//add a button titled "click me"
+
+$("#needy-button").click(function(){
+    totalNumber=totalNumber+1;
+
+$("#needy-button").html("You Died "+totalNumber+ " times...Better Luck Next Time!");
+
+});
+
+//when it is clicked 
+//add one to the total number
+
+// show the total number
+//on our button
+// show "clicked" total number "times"
+
+
+//a top limit on numbers
