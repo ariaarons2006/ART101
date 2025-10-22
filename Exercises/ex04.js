35b88de83ac4277fae85889f75345d2d42a195ed
akaarons <akaarons@ucsc.edu>
2025-10-21 18:47:22 -0700

    house

--- Exercises/ex04.js
+++ Exercises/ex04.js
@@ -1,20 +1,26 @@
 
 let totalNumber=0;
 let colors=["Orchid", "Coral", "Peachpuff", "Plum", "HotPink", "SeaGreen",];
+let mood;
 //add a button titled "click me"
 
 $("#needy-button").click(function(){
+ if (totalNumber < 5) { mood = "fresh and happy"; }
+   else if ((totalNumber >= 5) && (totalNumber < 10)) { mood = "keep pushing"; }
+   else { mood = "so tired!"; }
 
     let reminder=totalNumber % colors.length;
 
-    
-    $("#needy-button").html ("You Died "+ totalNumber +" Times... Better Luck Next Time! " +colors[reminder]);
+    $("#needy-button").html ("You Died "+ totalNumber +" Times... Better Luck Next Time! " +colors[reminder]+ " - " + mood);
 
 $("body").css ("background-color", colors[reminder]);
 
+if (colors[reminder]="Orchid")
+$("body").append("<img src='wilson.png'>");
+
     totalNumber=totalNumber+1;
 
-
+if (totalNumber == 6) {count=0; } 
 });
 
 
