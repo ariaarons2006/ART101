519fd67828e8c3484e13a0484a946d31c4a7a844
akaarons <akaarons@ucsc.edu>
2025-10-23 18:33:23 -0700

    mewo

---
+++ Exercises/ex05.js
@@ -0,0 +1,37 @@
+var count = 0;
+let colorCount = 0;
+var colors = ["Orchid", "Coral", "HotPink", "Plum"];
+
+
+function makeImage (imageName) {
+
+ if (colors[colorCount] == "Orchid") {
+       $("body").append("<img width=50 src='wilson.png'>");
+   }
+if (colors[colorCount] == "Coral") {
+       $("body").append("<img width=50 src='housemd.png'>");
+   }
+}
+function changeBackground (newColor) {
+    $("body").css("background-color", newColor)
+}
+$(".color-button").click(function () {
+changeBackground (this.id);
+
+});
+
+// the button part
+$("#needy-button").click(function () {
+   if (count < 5) { mood = "fresh and happy"; }
+   else if ((count >= 5) && (count < 10)) { mood = "keep pushing"; }
+   else { mood = "so tired"; }
+   $("#needy-button").html("Clicks: " + count + " Color: " + colors[colorCount] + " " + mood);
+   $("body").css("background-color", colors[colorCount]);
+
+
+   makeImage("housemd"); 
+
+   count = count + 1;
+   colorCount = colorCount + 1;
+   if (colorCount == 4) { colorCount = 0; }
+});
