5cd5124cdb9e3f343114b2d865fd4889837ace7a
akaarons <akaarons@ucsc.edu>
2025-10-30 17:32:01 -0700

    sure

---
+++ Exercises/task3.js
@@ -0,0 +1,82 @@
+let count=0
+let colorCount=0
+var colors=["Blue", "Pink", "Black","Red", "Yellow",];
+let words=["Sonic", "Amy", "Shadow", "Knuckles","Tails",];
+
+
+function changeBackground (newColor) {
+    $("body").css("background-color", newColor)
+}
+$(".color-button").click(function () {
+changeBackground (this.id);
+});
+
+function makeImage (imageName) {
+
+ if (colors[colorCount] == "Blue") {
+       $("body").append("<img width=50 src='sonic2.png'>");
+   }
+if (colors[colorCount] == "Pink") {
+       $("body").append("<img width=50 src='amy.webp'>");
+   }
+if (colors[colorCount] == "Black") {
+       $("body").append("<img width=50 src='shadow.webp'>");
+   }
+if (colors[colorCount] == "Red") {
+       $("body").append("<img width=50 src='knuckles.webp'>");
+   }
+ if (colors[colorCount] == "Yellow") {
+       $("body").append("<img width=50 src='tails.webp'>");
+   }
+}
+
+$("#Sonic").click(function () {
+
+   $("body").css("background-color", colors[0]);
+
+makeImage ("sonic2"); 
+   count=count+1
+colorCount= colorCount+1
+   if (colorCount == 5) { colorCount = 0; }
+
+});
+$("#Amy").click(function () {
+
+   $("body").css("background-color", colors[1]);
+makeImage("amy");
+
+   count=count+1
+colorCount= colorCount+1
+   if (colorCount == 5) { colorCount = 0; }
+
+});
+$("#Shadow").click(function () {
+
+   $("body").css("background-color", colors[2]);
+makeImage("shadow");
+
+   count=count+1
+colorCount= colorCount+1
+   if (colorCount == 5) { colorCount = 0; }
+
+});
+$("#Knuckles").click(function () {
+
+   $("body").css("background-color", colors[3]);
+makeImage("knuckles");
+
+   count=count+1
+colorCount= colorCount+1
+   if (colorCount == 5) { colorCount = 0; }
+
+});
+$("#Tails").click(function () {
+
+   $("body").css("background-color", colors[4]);
+makeImage("tails");
+
+   count=count+1
+colorCount= colorCount+1
+   if (colorCount == 5) { colorCount = 0; }
+
+});
